64e5bde5a9030baf2f272c65c4c2bb7032f40ab4
jordanadedeji <jdon4692@gmail.com>
2019-09-30 10:14:03 +0100

    new commit

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -9,6 +9,8 @@ class Takeaway
   def initialize
     @order = []
     @running_total = 0
+    @account_sid = 'AC0495cf6a55c4d6f7af1fe122a4f13ab4'
+    @auth_token = '31ef06db871adf98e4ebf5382153b430'
   end
 
   def list
@@ -29,25 +31,17 @@ class Takeaway
   def checkout(payment)
     fail "Your payment does not cover the balance" if payment != @running_total
 
-    account_sid = 'AC0495cf6a55c4d6f7af1fe122a4f13ab4'
-    auth_token = '31ef06db871adf98e4ebf5382153b430'
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    from = '+441392690338' # Your Twilio number
-    to = '+447429188123' # Your mobile phone number
     time = Time.new + 3600 # delivery time
-    @message = client.messages.create(
-    from: from,
-    to: to,
-    body: "Thank you! Your order was placed and will be delivered before #{time.strftime("%k:%M")}"
-    )
+    client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    message = client.messages.create(from: '+441392690338', to: '+447429188123',
+              body: "Thank you! Your order was placed and will be delivered before #{time.strftime("%k:%M")}")
   end
 end
 
-menu = Takeaway.new
-menu.list
-menu.select("Pizza", 4)
-menu.select("Lasagne", 1)
-puts menu.running_total
-puts menu.order
-puts menu.checkout(30)
+# menu = Takeaway.new
+# menu.list
+# menu.select("Pizza", 4)
+# menu.select("Lasagne", 1)
+# puts menu.running_total
+# puts menu.order
+# puts menu.checkout(20)
